9d28d7ce3d7ecf6519af0a04a5414d81a23e05c5
Jeremiah King <usaxray25@gmail.com>
2022-05-05 12:53:40 -0500

    add prettier formatter

--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,76 @@
-      //student array
-const students =[ 
- { id: 1,
-  name: 'Harry Potter',
-  house: 'Gryffendor',
-  crest: 'https://p.kindpng.com/picc/s/474-4746582_harry-potter-gryffindor-baby-bib-cotton-velcro-fastener.png',
-  expelled : false
+//student array
+const students = [
+  {
+    id: 1,
+    name: "Harry Potter",
+    house: "Gryffendor",
+    crest:
+      "https://p.kindpng.com/picc/s/474-4746582_harry-potter-gryffindor-baby-bib-cotton-velcro-fastener.png",
+    expelled: false,
   },
- { id: 2,
-   name: 'Newt Scammander',
-   house: 'Hufflepuff',
-   crest: 'https://m.media-amazon.com/images/I/71SHyMkX6gL._AC_SL1200_.jpg',
-   expelled : false
+  {
+    id: 2,
+    name: "Newt Scammander",
+    house: "Hufflepuff",
+    crest: "https://m.media-amazon.com/images/I/71SHyMkX6gL._AC_SL1200_.jpg",
+    expelled: false,
   },
- { id: 3,
-   name: 'Luna Lovegood',
-   house: 'Ravenclaw',
-   crest: 'https://www.seekpng.com/png/detail/184-1840811_ravenclaw-crest-harry-potter-harry-potter-ravenclaw-house.png',
-   expelled : false
+  {
+    id: 3,
+    name: "Luna Lovegood",
+    house: "Ravenclaw",
+    crest:
+      "https://www.seekpng.com/png/detail/184-1840811_ravenclaw-crest-harry-potter-harry-potter-ravenclaw-house.png",
+    expelled: false,
   },
- { id: 4,
-   name: 'Draco Malfoy',
-   house: 'Slytherin',
-   crest: 'https://p.kindpng.com/picc/s/154-1549411_slytherin-crest-black-and-white-hd-png-download.png',
-   expelled : false
+  {
+    id: 4,
+    name: "Draco Malfoy",
+    house: "Slytherin",
+    crest:
+      "https://p.kindpng.com/picc/s/154-1549411_slytherin-crest-black-and-white-hd-png-download.png",
+    expelled: false,
   },
-  { id: 5,
-    name: 'Tom Riddle',
-    house: 'Slytherin',
-    crest: 'https://p.kindpng.com/picc/s/154-1549411_slytherin-crest-black-and-white-hd-png-download.png',
-    expelled : true
-   } ]
-    //stores new students
-   const newStudents = [];
-   //will house expelled student array
-   const voldArmy = [];
-        //houses array    
-    // const houseSort =[{
-    //   house: 'Gryffendor',
-    // crest: "./assets/images/hogwarts-logo.png",
-    // },
-    // { house:'Ravenclaw',
-    //    houseImage: './images.ravenclaw.jpg',
-    // },
-    // { house:'Hufflepuff',
-    //   houseImage: './images.hufflepuff.jpg',
-    // },
-    // { house: 'Slytherin',
-    //   houseImage: './images.slytherin.jpg',
-    // } ]
-    // const item = houseSort[Math.floor(Math.random() * houseSort.length)]; //add this function to form?
-      //utility function
-    const renderToDom = (divId, textToRender) => {
-      const selectedElement = document.querySelector(divId);
-      selectedElement.innerHTML = textToRender;
-    };
+  {
+    id: 5,
+    name: "Tom Riddle",
+    house: "Slytherin",
+    crest:
+      "https://p.kindpng.com/picc/s/154-1549411_slytherin-crest-black-and-white-hd-png-download.png",
+    expelled: true,
+  },
+];
+//stores new students
+const newStudents = [];
+//will house expelled student array
+const voldArmy = [];
+//houses array
+// const houseSort =[{
+//   house: 'Gryffendor',
+// crest: "./assets/images/hogwarts-logo.png",
+// },
+// { house:'Ravenclaw',
+//    houseImage: './images.ravenclaw.jpg',
+// },
+// { house:'Hufflepuff',
+//   houseImage: './images.hufflepuff.jpg',
+// },
+// { house: 'Slytherin',
+//   houseImage: './images.slytherin.jpg',
+// } ]
+// const item = houseSort[Math.floor(Math.random() * houseSort.length)]; //add this function to form?
+//utility function
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender;
+};
 
-      //empty domString
- let domString=''; 
- let domStringExpelled ='';
+//empty domString
+let domString = "";
+let domStringExpelled = "";
 
-      //welcome card
- const welcomeCard  = () => {   
- 
+//welcome card
+const welcomeCard = () => {
   domString = `
       <div class="sortingCard" style="width: 65rem;">
       <h1 class="welcome-title">Welcome To Hogwarts</h1>
@@ -88,12 +97,11 @@ const students =[
     </div>
   </div>`;
 
-  
-renderToDom('#welcomeContainer', domString);
-   } 
-const indStudent = document.querySelector("#indStudent");  
+  renderToDom("#welcomeContainer", domString);
+};
+const indStudent = document.querySelector("#indStudent");
 const filterButtons = () => {
-  domString=`
+  domString = `
   <div class="d-flex flex-wrap justify-content-between my-3">
     <button id="gryffendor-btn" class="btn btn-primary" type="button">Gryffendor</button>
     <button id="hufflepuff-btn" class="btn btn-primary" type="button">Hufflepuff</button>
@@ -103,21 +111,21 @@ const filterButtons = () => {
     <button id="voldy-btn" class="btn btn-primary" type="button">Voldys Crew</button>
   </div>
   `;
-renderToDom('#filterContainer', domString); 
-}
+  renderToDom("#filterContainer", domString);
+};
 
-const studentsId = () =>{
-  students.forEach((indStudent,index) => {
-    return indStudent.id = index + 1;  
-   });
-  };
-  console.log(students);
+const studentsId = () => {
+  students.forEach((indStudent, index) => {
+    return (indStudent.id = index + 1);
+  });
+};
+console.log(students);
 
-  const cardsOnDom = (array) =>{
-    let domString =""; 
-    // if(indStudent.delete === false){ 
-    for (const indStudent of array) {
-      domString += `
+const cardsOnDom = (array) => {
+  let domString = "";
+  // if(indStudent.delete === false){
+  for (const indStudent of array) {
+    domString += `
       <div class="domCard1" text-center">
       <header><h2 class="card-title">First Year</h2></header>
       <hr>
@@ -135,14 +143,13 @@ const studentsId = () =>{
       </div>
      
     </div>`;
-      // }
-   renderToDom('#addStudentContainer',domString);
-    }
-    const voldyOnDom = (array) =>{
-      let domStringExpelled =""; 
-      for (const exStudent of array){
-    
-        evilDomString += `<div class="domCard2" text-center">
+    // }
+    renderToDom("#addStudentContainer", domString);
+  }
+  const voldyOnDom = (array) => {
+    let domStringExpelled = "";
+    for (const exStudent of array) {
+      evilDomString += `<div class="domCard2" text-center">
         <header><h2 class="card-title">Student</h2></header>
         <hr>
         <div class="card-body">
@@ -150,20 +157,20 @@ const studentsId = () =>{
         <hr>
         <img class="card-img-top2" src="./assets/images/death-eater-mask.jpg" alt="Evil Has No Undoing...">
           </div> 
-      </div>`
-      }
-      }
-    renderToDom("#voldemortsArmy", domStringExpelled);
+      </div>`;
     }
+  };
+  renderToDom("#voldemortsArmy", domStringExpelled);
+};
 
 const hide = () => {
-  document.getElementById('addStudentContainer').style.display ="none";
-}
+  document.getElementById("addStudentContainer").style.display = "none";
+};
 const show = () => {
-  document.getElementById('addStudentContainer').style.display ="";
-}
+  document.getElementById("addStudentContainer").style.display = "";
+};
 
-const formInput =() => {
+const formInput = () => {
   let domString = `      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBegin" aria-expanded="false" aria-controls="collapseBegin">
   Begin
   </button>
@@ -181,78 +188,73 @@ const formInput =() => {
     </div>
   </div>
   </div>
-</form>`
-renderToDom('#addStudentContainer', domString);
-}
+</form>`;
+  renderToDom("#addStudentContainer", domString);
+};
 
 //Sort form code
 
-  
-
-  const eventListeners = () => {
+const eventListeners = () => {
   // const sortedFunction = () =>{
-    document.querySelector('#filterContainer').addEventListener('click', (e) => {
-  
+  document.querySelector("#filterContainer").addEventListener("click", (e) => {
     if (e.target.id === "clear-btn") {
       cardsOnDom(students);
-
-    } else if (e.target.id === 'gryffendor-btn') {
-        cardsOnDom(students.filter((firstYear) => firstYear.house=== 'Gryffendor'));
-      
+    } else if (e.target.id === "gryffendor-btn") {
+      cardsOnDom(
+        students.filter((firstYear) => firstYear.house === "Gryffendor")
+      );
     } else if (e.target.id === "ravenclaw-btn") {
-      cardsOnDom(students.filter((firstYear) => firstYear.house==='Ravenclaw'));
-
+      cardsOnDom(
+        students.filter((firstYear) => firstYear.house === "Ravenclaw")
+      );
     } else if (e.target.id === "hufflepuff-btn") {
-      cardsOnDom(students.filter((firstYear) => firstYear.house==='Hufflepuff'));
-    
-     } else if (e.target.id === "slytherin-btn") {
-      cardsOnDom(students.filter((firstYear) => firstYear.house==='Slytherin'));
+      cardsOnDom(
+        students.filter((firstYear) => firstYear.house === "Hufflepuff")
+      );
+    } else if (e.target.id === "slytherin-btn") {
+      cardsOnDom(
+        students.filter((firstYear) => firstYear.house === "Slytherin")
+      );
+    } else if (e.target.id === "voldy-btn") {
+      cardsOnDom(
+        students.filter((firstYear) => firstYear.house === "Slytherin")
+      );
+    }
+  });
+};
 
-     } else if (e.target.id === "voldy-btn") {
-        cardsOnDom(students.filter((firstYear) => firstYear.house==='Slytherin'));
-       }
-   });
-}
-
-  
-
-
-    
-
-  
-    document.querySelector('#addStudentContainer').addEventListener('click', (e) => {
-      if (e.target.id) {
-     
-    const [, id] = e.target.id.split("--");  
-        console.log(e.target.id);
+document
+  .querySelector("#addStudentContainer")
+  .addEventListener("click", (e) => {
+    if (e.target.id) {
+      const [, id] = e.target.id.split("--");
+      console.log(e.target.id);
       //find the index of the object in the array
-      
-    const index = students.findIndex ((taco)=> taco.id === Number(id));
-        if (e.target.id.includes('delete')) { //checks to see if delete id matches before using splice method
-          voldArmy.push(students.splice(index, 1));   
-          cardsOnDom(students);
+
+      const index = students.findIndex((taco) => taco.id === Number(id));
+      if (e.target.id.includes("delete")) {
+        //checks to see if delete id matches before using splice method
+        voldArmy.push(students.splice(index, 1));
+        cardsOnDom(students);
       }
     }
   });
-  
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault(); 
 
- const newStudentObj = {  
-  name: document.querySelector('#name').value,
-  }
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
- cardsOnDom(students);
- students.push(newStudentObj);
- studentsId(newStudentObj);
- form.reset();
+  const newStudentObj = {
+    name: document.querySelector("#name").value,
+  };
+
+  cardsOnDom(students);
+  students.push(newStudentObj);
+  studentsId(newStudentObj);
+  form.reset();
 });
- hide();
- show();
- 
-
-
+hide();
+show();
 
 const startApp = () => {
   welcomeCard();
@@ -261,7 +263,7 @@ const startApp = () => {
   // sortStudents();
   // voldyOnDom();
   studentsId(students);
-  eventListeners(); 
+  eventListeners();
   // form();
 };
 startApp();
